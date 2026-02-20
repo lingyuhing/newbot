f2dfe815a3eb93a01ab268145b144a59b06d8560
lingyuhing <3868786446@qq.com>
2026-02-20 21:16:16 +0800

    chore: 添加.gitignore并清理缓存文件和配置

--- src/config.py
+++
@@ -1,7 +0,0 @@
-USER_ID="1"
-LLM_MODEL="ark-code-latest"
-LLM_KEY="f50c58ba-950c-4c48-a876-bb1df95b519a"
-LLM_BASE_URL="https://ark.cn-beijing.volces.com/api/coding/v3"
-ROOT_DIR="/home/linyuxin/Desktop/newbot/work"
-SKILL_DIR=["/home/linyuxin/Desktop/newbot/work/skills"]
-VIRTUAL_MODE=False
